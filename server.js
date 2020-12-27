1769d15ab53792d868e5b0889d488570b7ad88f2
Kevin Liang <kevinman003@gmail.com>
2020-12-27 14:56:56 -0800

    Use static

--- server.js
+++ server.js
@@ -23,13 +23,19 @@ const socketConnection = require('./socket');
 
 socketConnection(io);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 // Starting server
 const PORT = process.env.PORT || 5000;
