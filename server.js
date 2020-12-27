27f81f1a6fd9274290f518a2e1a1e5a985495606
Kevin Liang <kevinman003@gmail.com>
2020-12-27 14:36:09 -0800

    Add postbuild scripts

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ const socketConnection = require('./socket');
 
 socketConnection(io);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // Starting server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
